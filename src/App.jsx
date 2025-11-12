b8af3bd4a5a4494e2dfd68d296a01cadc6825c19
Ruslan Chmir <ruslan.chmir@panda-ventures.net>
2025-11-12 12:20:50 +0100

    added test gelInfo func

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,13 @@ function App() {
   const completedCount = todos.filter((todo) => todo.completed).length
   const totalCount = todos.length
 
+  const gelInfo = () => {
+    return {
+      completedCount: todos.filter((todo) => todo.completed).length,
+      totalCount: todos.length,
+    }
+  }
+
   console.log('completedCount', completedCount, 'totalCount', totalCount, 'todos', todos);
 
   return (
